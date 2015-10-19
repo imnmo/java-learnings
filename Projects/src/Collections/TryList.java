504ace1cd8fd839aeed59030e5a816ba6ba6afbb
mohamedi <mohamed.imran.noor.mohamed@ericsson.com>
2015-10-19 20:06:34 +0200

    play around with Lists

---
+++ Projects/src/Collections/TryList.java
@@ -0,0 +1,31 @@
+package Collections;
+
+import java.util.*;
+
+public class TryList {
+
+
+
+
+
+	public static void main(String [] args){
+
+
+		List<Integer> myList = new ArrayList<>();
+
+		myList.add(2);
+		myList.add(4);
+		myList.add(5);
+		myList.add(6);
+
+		for (Integer i: myList)
+		{
+
+			System.out.println(i);
+
+		}
+
+
+	}
+
+}
