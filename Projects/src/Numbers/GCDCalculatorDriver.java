a0dde50f2eeeb5232a3757c8092da2b42edbaad7
mohamedi <mohamed.imran.noor.mohamed@ericsson.com>
2015-10-27 21:26:59 +0100

    GCD algorithm added

---
+++ Projects/src/Numbers/GCDCalculatorDriver.java
@@ -0,0 +1,10 @@
+package Numbers;
+
+public class GCDCalculatorDriver {
+
+
+	public static void main(String [] args){
+		GCDCalculator myObj = new GCDCalculator();
+		System.out.println(myObj.GCDCalculatorValue(2, 1));
+	}
+}
