357b696bfa79b66e75a99ab79a19a52c77950c71
mohamedi <mohamed.imran.noor.mohamed@ericsson.com>
2015-10-29 21:05:02 +0100

    gcd

--- Projects/src/Numbers/GCDCalculator.java
+++ Projects/src/Numbers/GCDCalculator.java
@@ -11,7 +11,6 @@ package Numbers;
 
 public class GCDCalculator {
 	public int GCDCalculatorValue(int a, int b){
-
 		if(b == 0) return a;
 		int remainder = a%b ;
 		return (GCDCalculatorValue(b, remainder));
--- Projects/src/Numbers/GCDCalculatorDriver.java
+++ Projects/src/Numbers/GCDCalculatorDriver.java
@@ -5,6 +5,9 @@ public class GCDCalculatorDriver {
 
 	public static void main(String [] args){
 		GCDCalculator myObj = new GCDCalculator();
+		//Change the values for other inputs
 		System.out.println(myObj.GCDCalculatorValue(2, 1));
+
+		String.format("Hi my name is ");
 	}
 }
