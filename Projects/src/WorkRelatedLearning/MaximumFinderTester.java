48abc9aea8109eb1a4ad2cae83b6325797bde20f
mohamedi <mohamed.imran.noor.mohamed@ericsson.com>
2015-10-31 15:39:58 +0100

    Add maximum finder

---
+++ Projects/src/WorkRelatedLearning/MaximumFinderTester.java
@@ -0,0 +1,26 @@
+package WorkRelatedLearning;
+
+import java.util.*;
+
+public class MaximumFinderTester {
+
+
+    public static void main(String[] args){
+
+        MaximumFinder myMaximumFinder = new MaximumFinder();
+        // ArrayList version
+        //List<Integer> myList = new ArrayList<Integer>();
+        //LinkedList
+        List<Integer> myList = new LinkedList<Integer>();
+
+        myList.add(1);
+        myList.add(100);
+        myList.add(156);
+        myList.add(1000);
+        myList.add(159);
+        myList.add(100);
+
+        myMaximumFinder.maxFinder(myList);
+
+    }
+}
