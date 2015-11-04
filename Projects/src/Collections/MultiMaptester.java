2668d71a3199bc134a4eec48fcce94bb4a1cc528
mohamedi <mohamed.imran.noor.mohamed@ericsson.com>
2015-11-04 22:34:09 +0100

    MultiMap

---
+++ Projects/src/Collections/MultiMaptester.java
@@ -0,0 +1,14 @@
+package Collections;
+
+public class MultiMaptester {
+
+    public static void main(String[] args) {
+
+        MultiMap myObj = new MultiMap();
+        myObj.setValues();
+        myObj.getValues();
+
+
+    }
+
+}
