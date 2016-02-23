de6e4a9b7ad03e1ffd2db20938e0bd0bad593f09
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-02-23 22:49:04 +0100

    static method usage

---
+++ Projects/src/StaticDemo.java
@@ -0,0 +1,11 @@
+/**
+ * This is just a demo for static methods
+ * @author enomoha
+ *
+ */
+public class StaticDemo {
+    public static void PrintSomeStuff(){
+        System.out.println("Just trying a print somethinsg");
+    }
+
+}
---
+++ Projects/src/StaticDemoTester.java
@@ -0,0 +1,9 @@
+
+public class StaticDemoTester {
+
+    public static void main(String[] args) {
+        //You see I hate new() objects.........
+        StaticDemo.PrintSomeStuff();
+    }
+
+}
