d175356b459d3dec0b3acbe4546cb53a72789075
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-02-26 20:34:10 +0100

    Add scratchpad

---
+++ Projects/src/ScratchPad.java
@@ -0,0 +1,19 @@
+
+/**
+ * This class is basically a scratch pad for fuzzy things which need to tested sytactically in Java
+ * @author enomoha
+ *
+ */
+public class ScratchPad
+{
+
+    public static void main(String[] args) {
+
+        String myString = null ; //What happens if the string is assigned null
+        System.out.printf("myString: %s", myString);
+        myString.equals(myString);
+
+
+    }
+
+}
