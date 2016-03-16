1f7cb1820671fd48e7bae0fcd8d8d1bfdc371918
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-03-16 16:15:34 +0100

    Static Blocks

--- Projects/src/ScratchPad.java
+++ Projects/src/ScratchPad.java
@@ -7,11 +7,28 @@
 public class ScratchPad
 {
 
+    /**
+     * just an smaple method
+     */
+    private void sampleMethod()
+    {
+        System.out.println("here we go...");
+    }
+    
+    
+    //Add some static blocks and it acts a driver to load the stuff
+    static
+    {
+        System.out.println("just try some static blocks");
+        ScratchPad myScratchPad = new ScratchPad();
+        myScratchPad.sampleMethod();
+    }
+    
     public static void main(String[] args) {
 
-        String myString = null ; //What happens if the string is assigned null
-        System.out.printf("myString: %s", myString);
-        myString.equals(myString);
+//        String myString = null ; //What happens if the string is assigned null
+//        System.out.printf("myString: %s", myString);
+//        myString.equals(myString);
 
 
     }
