99a4b1afd34dd94a09922fd2182f656cc0634850
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-04-21 10:28:27 +0200

    Add adapter pattern

---
+++ Projects/src/com/mylearnings/adapter/pattern/FishingBoat.java
@@ -0,0 +1,23 @@
+package com.mylearnings.adapter.pattern;
+
+/**
+ * This is a plain commericial boat only used for shipping purposes(Adaptee class)
+ * @author enomoha
+ *
+ */
+
+public class FishingBoat
+{
+    
+    public void sail()
+    {
+        System.out.println("I'm sailing using Fishing boat engine");
+    }
+    
+
+    public void Fish()
+    {
+        System.out.println("lets fish some- Atlansiches Fische ");
+    }
+
+}
