d090a717f7da430e898f3d245c69a69de97e0f82
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-09-20 17:45:12 +0200

    Add BST Struct

---
+++ mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/DataStructures/BinaryTrees.java
@@ -0,0 +1,51 @@
+package com.Imran.learnings.DataStructures;
+
+/**
+ * An experimental tree in specific binary search tree class speaking about the
+ */
+
+
+public class BinaryTrees
+{
+    Node root = null; //the root node
+
+
+    /**
+     * The logic for the insertion of the key into the tree
+     *
+     * @param pKey
+     */
+
+    public void insert(int pKey)
+    {
+
+    }
+
+
+    /**
+     * The logic to find the search within the BST
+     *
+     * @param pKey
+     */
+    public void find(int pKey)
+    {
+
+    }
+
+    /**
+     * The logic to delete the key from the BST
+     */
+
+    public void delete(int pKey)
+    {
+    }
+
+    /**
+     * The traverse logic for the tree
+     * inorder, preorder and post order
+     */
+    public static void traverse()
+    {
+    }
+
+}
