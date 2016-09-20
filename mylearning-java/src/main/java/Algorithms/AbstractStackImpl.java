a38d335bc696ee68c2398d5ccf3b08825061ac53
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-09-20 13:46:43 +0200

    Add algos

--- mylearning-java/src/main/java/Algorithms/AbstractStackImpl.java
+++ mylearning-java/src/main/java/Algorithms/AbstractStackImpl.java
@@ -1,29 +1,32 @@
 package Algorithms;
 
 /**
- * Generic parametrized stack implementation of the stack using the 
- * 
- *  For test now its with the String based implementation
- *  kind of bare bone with no JavaDocs
- * This class has implements Iterable interface 
+ * Generic parametrized stack implementation of the stack using the
+ * <p>
+ * For test now its with the String based implementation
+ * kind of bare bone with no JavaDocs
+ * This class has implements Iterable interface
  */
 //TODO the pop with loitering and GC stuff na
 @SuppressWarnings("hiding")
-public class AbstractStackImpl<Object> implements AbstractStackI{
+public class AbstractStackImpl<Object> implements AbstractStackI
+{
 
     protected String myStackArray[]; //Stack holding the array
     protected int positionOfElementinStack; //Holds the position  of elements to be added in stack
 
     //A default constructor taking the intial capacity for stack allocation  
-    public AbstractStackImpl(int stackCapacity) {
+    public AbstractStackImpl(int stackCapacity)
+    {
         myStackArray = new String[stackCapacity];
     }
 
     //basically move the position and add the element
     @Override
-    public void pushElements(String Item) {
-        
-        if(isFull())
+    public void pushElements(String Item)
+    {
+
+        if (isFull())
         {
             //do stack resizing
             resize(4 * myStackArray.length);
@@ -33,37 +36,43 @@ public class AbstractStackImpl<Object> implements AbstractStackI{
 
     //basically move the position of the array holding
     @Override
-    public String popElements() {
-        if(isEmpty()){
+    public String popElements()
+    {
+        if (isEmpty())
+        {
             throw new RuntimeException("Stack underflow dude!! ");
         }
 //        myStackArray[positionOfElementinStack] = null; 
-        return myStackArray[positionOfElementinStack--];
+        return myStackArray[0];
     }
 
     @Override
-    public Boolean isEmpty() {
+    public Boolean isEmpty()
+    {
         if (positionOfElementinStack == 0)
             return true;
         return false;
     }
 
     @Override
-    public void displayElements() {
+    public void displayElements()
+    {
     }
 
-    
+
     //Peeks into the top elements of the Stack
     @Override
-    public String peekElements() {
+    public String peekElements()
+    {
         int maximumStackValue = myStackArray.length;
         return myStackArray[maximumStackValue];
     }
-    
+
     //just takes the length and checks whether is full or not
     @Override
-    public Boolean isFull() {
-        if (myStackArray.length ==  positionOfElementinStack)
+    public Boolean isFull()
+    {
+        if (myStackArray.length == positionOfElementinStack)
             return true;
         return false;
     }
@@ -71,25 +80,28 @@ public class AbstractStackImpl<Object> implements AbstractStackI{
     /**
      * returns the size of the stack
      */
-    
+
     @Override
-    public int sizeOfStack() {
+    public int sizeOfStack()
+    {
         return positionOfElementinStack;
     }
 
     /**
-     * Resizes the Array to use for extended length 
+     * Resizes the Array to use for extended length
      */
     @Override
-    public void resize(int maximumReSizeValue) {
+    public void resize(int maximumReSizeValue)
+    {
         //Allocate a new array and copy the contents to new temp
         //array
         String[] temp = new String[maximumReSizeValue];
-        for(int i=0; i < positionOfElementinStack; i++){
+        for (int i = 0; i < positionOfElementinStack; i++)
+        {
             temp[i] = myStackArray[i];
         }
         //now just copy the contents
         myStackArray = temp;
     }
-   
+
 }
---
+++ mylearning-java/src/main/java/Collections/FindDup.java
@@ -0,0 +1,33 @@
+package Collections;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by enomoha on 24.08.2016.
+ */
+public class FindDup
+{
+
+
+    public boolean findDuplicates(List<String> listContainingDuplicates)
+    {
+
+        Set<String> mySetWIthDuplicates = new HashSet<>();
+
+        boolean isPrsent = false;
+        for (String myString : listContainingDuplicates)
+
+        {
+            if (!mySetWIthDuplicates.add(myString))
+            {
+                isPrsent = true;
+
+            }
+
+        }
+        return isPrsent;
+    }
+
+}
