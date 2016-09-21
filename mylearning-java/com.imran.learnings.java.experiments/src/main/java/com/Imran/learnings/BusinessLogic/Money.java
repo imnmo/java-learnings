850418d3d28f43908305c8586de99a67675dfb14
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-09-21 12:16:05 +0200

    Add insert and find BST

--- mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/BusinessLogic/Money.java
+++ mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/BusinessLogic/Money.java
@@ -5,27 +5,17 @@ package com.Imran.learnings.BusinessLogic;
  */
 public class Money
 {
-
-
-    public boolean isPsitive()
+    public boolean isPositive()
     {
         Integer myInteger = Integer.valueOf(7);
-
         int myINt = 0;
-
-
         System.out.println(Math.signum(myINt) > 0);
-
         return Math.signum(myINt) > 0;
-
     }
 
     public static void main(String... args)
     {
-
         Money myMoney = new Money();
-        System.out.println(myMoney.isPsitive());
-
-
+        System.out.println(myMoney.isPositive());
     }
 }
--- mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/DataStructures/BinaryTrees.java
+++ mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/DataStructures/BinaryTrees.java
@@ -9,7 +9,6 @@ public class BinaryTrees
 {
     Node root = null; //the root node
 
-
     /**
      * The logic for the insertion of the key into the tree
      *
@@ -18,17 +17,97 @@ public class BinaryTrees
 
     public void insert(int pKey)
     {
-
+        Node newNode = initializeNode(pKey);
+        Node current;
+        //No root node is existing then update it to root
+        if (root == null)
+        {
+            root = newNode;
+        }
+        else
+        {
+            current = root;
+            //Keep inserting
+            insertItems(pKey, current, newNode);
+        }
     }
 
+    /**
+     * Creates and initializes the node
+     *
+     * @param pKey
+     * @return
+     */
+    private Node initializeNode(int pKey)
+    {
+        Node parent;
+        Node current;
+        Node newNode = new Node();
+        newNode.data = pKey; //Update the newly created node witn the key or data 
+        return newNode;
+    }
+
+    /**
+     * Insert the elements into the tree
+     *
+     * @param pKey
+     * @param current
+     * @param newNode
+     */
+    private void insertItems(int pKey, Node current, Node newNode)
+    {
+        Node parent;
+        while (true)
+        {
+            //Update the parent
+            parent = current;
+            if (pKey < current.data)
+            {
+                current = current.leftChild;
+                if (current == null)
+                {
+                    parent.leftChild = newNode;
+                    return;
+                }
+            }
+            else if (pKey > current.data)
+            {
+                current = current.rightChild;
+                if (current == null)
+                {
+
+                    parent.rightChild = newNode;
+                    return;
+                }
+            }
+        }
+    }
 
     /**
      * The logic to find the search within the BST
      *
      * @param pKey
      */
-    public void find(int pKey)
+
+    public Node find(int pKey)
     {
+        Node current = root;
+        while (current.data != pKey)
+        {
+            if (pKey < current.data)
+            {
+                current = current.leftChild;
+            }
+            if (pKey > current.data)
+            {
+                current = current.rightChild;
+            }
+            if (current == null)
+            {
+                return null;
+            }
+        }
+        return current;
 
     }
 
