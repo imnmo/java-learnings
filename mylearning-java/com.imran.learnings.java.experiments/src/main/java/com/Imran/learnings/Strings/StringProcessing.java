8715c04ead5d864e9ee25ddad014a023560e9a0a
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-10-19 15:23:08 +0200

    Add string processing

--- mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/Strings/StringProcessing.java
+++ mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/Strings/StringProcessing.java
@@ -3,6 +3,7 @@ package com.Imran.learnings.Strings;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by ${User} on 14.10.2016.
@@ -17,7 +18,24 @@ public class StringProcessing
         List<String> split = new ArrayList<>();
         split.addAll(Arrays.asList(myString.split("/")));
 
+        int myItemSIze = split.size();
+
+        //Everything with iterator
+        ListIterator myIterator = split.listIterator();
+        while (myIterator.hasNext())
+        {
+            if (myItemSIze % 2 == 0)
+            {
+                int position = split.size();
+                Object element = myIterator.next();
+                if (myIterator.nextIndex() == position)
+                {
+                    myIterator.set("v2" + element);
+                }
+            }
+        }
         System.out.println(split);
+
 
         for (String item : split)
         {
@@ -31,13 +49,21 @@ public class StringProcessing
 
         }
 
+        //Substring operation to uppercase
+        String myProcessId = "lingo";
+        String Bingo = myProcessId.substring(0, 1).toUpperCase() + myProcessId.substring(1);
+        System.out.println(Bingo);
+
+        //checks if the particular char is upper or lower
+        Character.isLowerCase(myString.charAt(0));
+        //reconstruc´t the string 
         StringBuilder stringBuilder = new StringBuilder();
         for (String item : split)
         {
             stringBuilder.append(item).append("/");
         }
 
-        System.out.println(stringBuilder.toString());
+        System.out.println(stringBuilder);
 
     }
 }
