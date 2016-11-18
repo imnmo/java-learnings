ace28cc1249d17194c8277c1665d7ecd9e5ab291
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-11-18 08:35:13 +0100

    add insert-delete

--- mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/doublylinkedlists/DoublyLinkedLists.java
+++ mylearning-java/com.imran.learnings.java.experiments/src/main/java/com/Imran/learnings/doublylinkedlists/DoublyLinkedLists.java
@@ -33,4 +33,60 @@ public class DoublyLinkedLists
         newLink.next = first; // newLink --> old first
         first = newLink; // first --> newLink
     }
+
+    public void insertLast(int dd) // insert at end of list
+    {
+        Node newLink = new Node(dd); // make new link
+        if (isEmpty()) // if empty list,
+            first = newLink; // first --> newLink
+        else
+        {
+            last.next = newLink; // old last --> newLink
+            newLink.previous = last; // old last <-- newLink
+        }
+        last = newLink; // newLink <-- last
+    }
+
+    public void displayForward()
+    {
+        Node current = first; // start at beginning
+        while (current != null) // until end of list,
+        {
+            current.displayLink(); // display data
+            current = current.next; // move to next link
+        }
+    }
+
+    // -------------------------------------------------------------
+    public void displayBackward()
+    {
+        Node current = last; // start at end
+        while (current != null) // until start of list,
+        {
+            current.displayLink(); // display data
+            current = current.previous;
+        }
+    }
+
+    public Node deleteFirst() // delete first link
+    { // (assumes non-empty list)
+        Node temp = first;
+        if (first.next == null) // if only one item
+            last = null; // null <-- last
+        else
+            first.next.previous = null; // null <-- old next
+        first = first.next; // first --> old next
+        return temp;
+    }
+
+    public Node deleteLast() // delete last link
+    { // (assumes non-empty list)
+        Node temp = last;
+        if (first.next == null) // if only one item
+            first = null; // first --> null
+        else
+            last.previous.next = null; // old previous --> null
+        last = last.previous; // old previous <-- last
+        return temp;
+    }
 }
