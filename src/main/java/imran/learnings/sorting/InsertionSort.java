e6057ac251fb1aa7d64d77cafc4acf238e0147cf
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2016-12-18 10:39:17 +0100

    insertionSOrt

--- src/main/java/imran/learnings/sorting/InsertionSort.java
+++ src/main/java/imran/learnings/sorting/InsertionSort.java
@@ -1,8 +1,33 @@
 package imran.learnings.sorting;
 
+import java.util.Arrays;
+
 /**
  * this class demonstrates the insertion sort
  */
 public class InsertionSort
 {
+    public int[] insertionSort(int[] arrayToSort)
+    {
+        for (int in = 1; in < arrayToSort.length - 1; in++)
+        {
+            int value = arrayToSort[in];
+            int hole = in;
+            while (hole > 0 && arrayToSort[hole - 1] > value)
+            {
+                arrayToSort[hole] = arrayToSort[hole - 1];
+                --hole;
+            }
+            arrayToSort[hole] = value;
+        }
+
+        return arrayToSort;
+    }
+
+    public static void main(String args[])
+    {
+        int[] myArray = {1, 4, 5, 2, 3, 0};
+        InsertionSort insertionSort = new InsertionSort();
+        System.out.print(Arrays.toString(insertionSort.insertionSort(myArray)));
+    }
 }
