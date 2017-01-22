f3902829fe13e40f0d852ace5935c0c590a459fc
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2017-01-22 17:49:11 +0100

    add edges and vertex

---
+++ src/main/java/imran/learnings/datastructure/graphs/Edge.java
@@ -0,0 +1,97 @@
+package imran.learnings.datastructure.graphs;
+
+/**
+ * Created by ${User} on 22.01.2017.
+ */
+class Edge<T>
+{
+    private boolean isDirected = false;
+    private Vertex<T> vertex1;
+    private Vertex<T> vertex2;
+    private int weight;
+
+    Edge(Vertex<T> vertex1, Vertex<T> vertex2)
+    {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+    }
+
+    Edge(Vertex<T> vertex1, Vertex<T> vertex2, boolean isDirected, int weight)
+    {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.weight = weight;
+        this.isDirected = isDirected;
+    }
+
+    Edge(Vertex<T> vertex1, Vertex<T> vertex2, boolean isDirected)
+    {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.isDirected = isDirected;
+    }
+
+    Vertex<T> getVertex1()
+    {
+        return vertex1;
+    }
+
+    Vertex<T> getVertex2()
+    {
+        return vertex2;
+    }
+
+    int getWeight()
+    {
+        return weight;
+    }
+
+    public boolean isDirected()
+    {
+        return isDirected;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((vertex1 == null) ? 0 : vertex1.hashCode());
+        result = prime * result + ((vertex2 == null) ? 0 : vertex2.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Edge other = (Edge) obj;
+        if (vertex1 == null)
+        {
+            if (other.vertex1 != null)
+                return false;
+        }
+        else if (!vertex1.equals(other.vertex1))
+            return false;
+        if (vertex2 == null)
+        {
+            if (other.vertex2 != null)
+                return false;
+        }
+        else if (!vertex2.equals(other.vertex2))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Edge [isDirected=" + isDirected + ", vertex1=" + vertex1
+                + ", vertex2=" + vertex2 + ", weight=" + weight + "]";
+    }
+}
--- src/main/java/imran/learnings/datastructure/graphs/Vertex.java
+++ src/main/java/imran/learnings/datastructure/graphs/Vertex.java
@@ -1,17 +1,85 @@
 package imran.learnings.datastructure.graphs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The vertex or the node of the class
  */
-public class Vertex
+class Vertex<T>
 {
+    long id;
+    private T data;
+    private List<Edge<T>> edges = new ArrayList<>();
+    private List<Vertex<T>> adjacentVertex = new ArrayList<>();
 
-    public char label; // label (e.g. ‘A’)
-    public boolean wasVisited;
-
-    public Vertex(char lab) // constructor
+    Vertex(long id)
     {
-        label = lab;
-        wasVisited = false;
+        this.id = id;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setData(T data)
+    {
+        this.data = data;
+    }
+
+    public T getData()
+    {
+        return data;
+    }
+
+    public void addAdjacentVertex(Edge<T> e, Vertex<T> v)
+    {
+        edges.add(e);
+        adjacentVertex.add(v);
+    }
+
+    public String toString()
+    {
+        return String.valueOf(id);
+    }
+
+    public List<Vertex<T>> getAdjacentVertexes()
+    {
+        return adjacentVertex;
+    }
+
+    public List<Edge<T>> getEdges()
+    {
+        return edges;
+    }
+
+    public int getDegree()
+    {
+        return edges.size();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vertex other = (Vertex) obj;
+        if (id != other.id)
+            return false;
+        return true;
     }
 }
